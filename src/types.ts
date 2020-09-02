d6bbe86cb69042d21963c2c6e6f34c3d9c060c4f
Nujood <shawar.nujood@gmail.com>
2020-09-02 17:57:46 +0300

    neaten up the declaration

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,15 @@
-let myString: string;
-let num: number;
-let myBool: boolean;
-let myVar: any;
+let myString: string = "Tenkile" + " " + "Wenilkle".slice(0, 3);
+let num: number = 1;
+let myBool: boolean = true;
+let myVar: any = "whatever";
 
 // let strArr: string[];
 // let numArr: number[];
 // let boolArr: boolean[];
 
-let strArr: Array<string>;
-let numArr: Array<number>;
-let boolArr: Array<boolean>;
+let strArr: Array<string> = ["Nujood", "Here"];
+let numArr: Array<number> = [1, 2, 4];
+let boolArr: Array<boolean> = [true, false];
 
 //changing from  null or undefined wont make a difference
 let myVoid: void = undefined;
@@ -17,16 +17,7 @@ let myNull: null = null;
 let myUndefined: undefined = undefined;
 
 let strNumTuple: [string, number];
-
-myString = "Tenkile" + " " + "Wenilkle".slice(0, 3);
-num = 1;
-myBool = true;
-myVar = "whaterver type";
-
-strArr = ["Nujood", "Here"];
-numArr = [1, 2, 4];
-boolArr = [true, false];
-// hase to match the declared index exactly- you can  add after the pattern but not before
+// has to match the declared index exactly- you can  add after the pattern but not before
 strNumTuple = ["hello", 4];
 
 console.log(myString, num, myBool, strArr, numArr, boolArr, strNumTuple);
